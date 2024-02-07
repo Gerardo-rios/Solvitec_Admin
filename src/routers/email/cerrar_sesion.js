2032c7f2887f00fe510d2346744dfe02794157c9
jsgerardor <gerardo.rios@ioet.com>
2024-02-07 16:27:18 -0500

    logout fixed

--- src/routers/email/cerrar_sesion.js
+++ src/routers/email/cerrar_sesion.js
@@ -1,24 +1,21 @@
 const { auth } = require("../../firebase");
 const { signOut } = require("firebase/auth");
-const { isLogged } = require("../../auth/session_auth");
 const express = require("express");
 const router = express.Router();
 
-router.get("/logout", isLogged, async (req, res) => {
-  try {
-    const reponse = await signOut(auth);
-    console.log("Sesión cerrada", reponse);
+router.get("/logout", async (req, res) => {
+  signOut(auth).then(() => {
     res.clearCookie("user");
-    res.status(200);
-    res.end();
-    return;
-  } catch (error) {
+    res.status(200).redirect("/");
+  }).catch((error) => {
     console.error("Error al cerrar sesión:", error);
     res.status(500).render("error", {
       errorMessage: "Error al cerrar sesión: " + error.message,
       errorCode: error.code,
     });
-  }
+  }).finally(() => {
+    res.end();
+  });
 });
 
 module.exports = router;
